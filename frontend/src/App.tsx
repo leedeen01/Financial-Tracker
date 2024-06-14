4f5a31445107a7a84bce95d9349c8aa9ef4ef5a0
leedeen01 <lmaodeen@gmail.com>
2024-06-14 20:38:41 +0800

    added routes for different pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,20 +5,27 @@ import NavBar from "./components/Nav/NavBar.tsx";
 import { useEffect, useState } from "react";
 import { User } from "./models/user.ts";
 import * as ExpensesApi from "./network/expenses_api";
-import { Container } from "react-bootstrap";
 import Home from "./pages/Home.tsx";
 import HomeLoggedOut from "./pages/HomeLoggedOut.tsx";
+import { Route, Routes } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import Categories from "./pages/Categories.tsx";
+import Budget from "./pages/Budget.tsx";
+import Investment from "./pages/Investment.tsx";
+import Friends from "./pages/Friends.tsx";
 
 function App() {
   const [loggedInUser, setLoggedInUser] = useState<User | null>(null);
 
   const [showSignUp, setShowSignUp] = useState(false);
   const [showLogin, setShowLogin] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     async function fetchLoggedInUser() {
       try {
         const user = await ExpensesApi.getLoggedInUser();
+        navigate("/home");
         setLoggedInUser(user);
       } catch (error) {
         console.error(error);
@@ -34,18 +41,30 @@ function App() {
         loggedInUser={loggedInUser}
         onLoginClicked={() => setShowLogin(true)}
         onSignUpClicked={() => setShowSignUp(true)}
-        onLogoutSuccessful={() => setLoggedInUser(null)}
+        onLogoutSuccessful={() => {
+          navigate("/");
+          setLoggedInUser(null);
+        }}
       />
 
-      <Container>
-        <>{loggedInUser ? <Home /> : <HomeLoggedOut />}</>
-      </Container>
+      <div>
+        <Routes>
+          <Route index element={<HomeLoggedOut />}></Route>
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/categories" element={<Categories />}></Route>
+          <Route path="/budgets" element={<Budget />}></Route>
+          <Route path="/investments" element={<Investment />}></Route>
+          <Route path="/friends" element={<Friends />}></Route>
+        </Routes>
+      </div>
 
       {showSignUp && (
         <SignUp
           onDismiss={() => setShowSignUp(false)}
           onSignUpSuccessful={(user) => {
             setLoggedInUser(user);
+            navigate("/home");
+
             setShowSignUp(false);
           }}
         />
@@ -55,6 +74,8 @@ function App() {
           onDismiss={() => setShowLogin(false)}
           onLoginSuccessful={(user) => {
             setLoggedInUser(user);
+            navigate("/home");
+
             setShowLogin(false);
           }}
         />
--- frontend/src/components/Nav/NavBarLoggedInView.tsx
+++ frontend/src/components/Nav/NavBarLoggedInView.tsx
@@ -1,45 +1,38 @@
 import { User } from "../../models/user";
 import { Navbar, Button } from "react-bootstrap";
 import * as ExpensesApi from "../../network/expenses_api";
+import { useNavigate } from "react-router-dom";
 
 interface NavBarLoggedInViewProps {
-    user: User,
-    onLogoutSuccessful: () => void,
+  user: User;
+  onLogoutSuccessful: () => void;
 }
 
-const NavBarLoggedInView = ({user, onLogoutSuccessful}: NavBarLoggedInViewProps) => {
-    async function logout() {
-        try {
-            await ExpensesApi.logout();
-            onLogoutSuccessful();
-        } catch (error) {
-            console.error(error);
-            alert(error);
-        }
+const NavBarLoggedInView = ({
+  user,
+  onLogoutSuccessful,
+}: NavBarLoggedInViewProps) => {
+  async function logout() {
+    try {
+      await ExpensesApi.logout();
+      onLogoutSuccessful();
+    } catch (error) {
+      console.error(error);
+      alert(error);
     }
-    
-    return (
-        <>
-            <Navbar.Text className="mx-auto">
-                Welcome, {user.username}
-            </Navbar.Text>
-            <Button onClick={logout}>
-                Categories
-            </Button>
-            <Button onClick={logout}>
-                Budgets
-            </Button>
-            <Button onClick={logout}>
-                Investments
-            </Button>
-            <Button onClick={logout}>
-                Friends
-            </Button>
-            <Button onClick={logout}>
-                Log Out
-            </Button>
-        </>
-    );
-}
+  }
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <Navbar.Text className="mx-auto">Welcome, {user.username}</Navbar.Text>
+      <Button onClick={() => navigate("/categories")}> Categories</Button>
+      <Button onClick={() => navigate("/budgets")}>Budgets</Button>
+      <Button onClick={() => navigate("/investments")}>Investments</Button>
+      <Button onClick={() => navigate("/friends")}>Friends</Button>
+      <Button onClick={logout}>Log Out</Button>
+    </>
+  );
+};
 
 export default NavBarLoggedInView;
