8009589baa8d7cb6bde1543043abf284b390f7fc
FabianHeng <fabianheng@gmail.com>
2024-07-02 01:11:44 +0800

    updated timeout loader messages

--- frontend/src/components/loader/Loader.tsx
+++ frontend/src/components/loader/Loader.tsx
@@ -2,10 +2,20 @@ import { useState, useEffect } from 'react';
 import './Loader.css';
 
 const Loader = () => {
+  const [wait, setWait] = useState(false);
   const [timeoutReached, setTimeoutReached] = useState(false);
 
   useEffect(() => {
     const timer = setTimeout(() => {
+      setWait(true);
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setWait(false);
       setTimeoutReached(true);
     }, 60000);
 
@@ -20,6 +30,7 @@ const Loader = () => {
         </svg>
       </div>
       <br></br>
+      {wait && (<p className='text-center'>Attempting to fetch data, please wait...</p>)}
       {timeoutReached && (<p className='text-center'>Something is wrong... try logging in again.</p>)}
     </div>
   );
