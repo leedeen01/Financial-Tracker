b51fd23fc8ee9b7d587d1001504823778ee155a7
leedeen01 <lmaodeen@gmail.com>
2024-07-12 16:50:24 +0800

    edited budget

--- frontend/src/components/form/ColorInputField.tsx
+++ frontend/src/components/form/ColorInputField.tsx
@@ -1,6 +1,6 @@
-import { Form, Button, ButtonGroup } from "react-bootstrap";
+import { useState } from "react";
+import { Form } from "react-bootstrap";
 import { FieldError, RegisterOptions, UseFormRegister } from "react-hook-form";
-import { useState, useEffect } from "react";
 
 interface ColorInputFieldProps {
   name: string;
@@ -25,43 +25,32 @@ const ColorInputField = ({
 }: ColorInputFieldProps) => {
   const [selectedColor, setSelectedColor] = useState(defaultValue || options[0]);
 
-  useEffect(() => {
-    setSelectedColor(defaultValue || options[0]); // Reset selected color if defaultValue changes
-  }, [defaultValue, options]);
 
-  const handleColorChange = (color: string) => {
-    setSelectedColor(color);
-    // Update defaultValue of Form.Control to match selected color
-    const inputElement = document.getElementById(name + "-input") as HTMLInputElement;
-    if (inputElement) {
-      inputElement.value = color;
-    }
-  };
 
   return (
     <Form.Group className="mb-3" controlId={name + "-input"}>
       <div className="d-flex align-items-center justify-content-between mb-3">
         <Form.Label>{label}</Form.Label>
-        <Form.Control
-          type="color"
-          defaultValue={selectedColor} // Use selectedColor as defaultValue
-          {...props}
-          {...register(name, registerOptions)}
-          isInvalid={!!error}
-        />
-      </div>
-      <div className="overflow-x-auto">
-        <ButtonGroup className="mb-3">
-          {options.map((color) => (
-            <Button
-              key={color}
-              style={{ backgroundColor: color, borderColor: color }}
-              onClick={() => handleColorChange(color)}
-              className={selectedColor === color ? 'color-button-active' : ''}
-            >
-            </Button>
-          ))}
-        </ButtonGroup>
+        {/* <div className="overflow-x-auto">
+          <ButtonGroup className="mb-3">
+            {options.map((color) => (
+              <Button
+                key={color}
+                style={{ backgroundColor: color, borderColor: color }}
+                onClick={() => handleColorChange(color)}
+                className={selectedColor === color ? 'color-button-active' : ''}
+              />
+            ))}
+          </ButtonGroup>
+        </div> */}
+          <Form.Control
+            type="color"
+            value={selectedColor}
+            {...props}
+            {...register(name, registerOptions)}
+            onChange={(e) => setSelectedColor(e.target.value)}
+            isInvalid={!!error}
+          />
       </div>
       <Form.Control.Feedback type="invalid">
         {error?.message}
