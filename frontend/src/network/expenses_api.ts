2c7f204676e5e149de7390bc9b77d169ba1605bf
leedeen01 <lmaodeen@gmail.com>
2024-07-15 13:27:17 +0800

    changed background colour

--- frontend/src/network/expenses_api.ts
+++ frontend/src/network/expenses_api.ts
@@ -4,8 +4,8 @@ import { User } from "../models/user";
 import { Category } from "../models/category";
 import { Account } from "../models/account";
 
-const website = "http://localhost:6969";
-// const website = "https://financial-tracker-mtpk.onrender.com";
+// const website = "http://localhost:6969";
+const website = "https://financial-tracker-mtpk.onrender.com";
 
 //login/signup/logout related
 async function fetchData(input: RequestInfo, init?: RequestInit) {
